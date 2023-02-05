a0f9a0fc82f56d4eaaef5fba63950bdf6f6fc877
JackNoordhuis <jackjnoordhuis@gmail.com>
2023-02-05 18:27:23 +1100

    AutoInvLoader::loadConfiguration(): make phpstan happy
    * Properly check/sanitise config inputs to reduce errors caused by mis-configuration.

--- src/AutoInvLoader.php
+++ src/AutoInvLoader.php
@@ -18,8 +18,9 @@ use JackNoordhuis\AutoInv\Listener\EntityExplosionPickup;
 use JackNoordhuis\AutoInv\Listener\PlayerDeathPickup;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginException;
 use pocketmine\utils\TextFormat;
-use function is_bool;
+use function is_array;
 use function is_string;
 use function strtolower;
 use function yaml_parse_file;
@@ -28,6 +29,16 @@ use const DIRECTORY_SEPARATOR;
 class AutoInvLoader extends PluginBase {
 
 	private const SETTINGS_CONFIG = 'Settings.yml';
+
+	/**
+	 * Mapping of config option keys to listener classes.
+	 */
+	private const OPTIONAL_LISTENERS = [
+		"block-break" => BlockBreakAutoPickup::class,
+		"entity-death" => EntityDeathAutoPickup::class,
+		"player-death" => PlayerDeathPickup::class,
+		"entity-explosion" => EntityExplosionPickup::class
+	];
 
 	/**
 	 * Called when the plugin is loaded, before calling onEnable()
@@ -40,44 +51,61 @@ class AutoInvLoader extends PluginBase {
 	 * Called when the plugin is enabled
 	 */
 	protected function onEnable() : void {
-		$this->loadConfiguration(yaml_parse_file(
-			$this->getDataFolder() . DIRECTORY_SEPARATOR . self::SETTINGS_CONFIG
-		));
+		$settings = yaml_parse_file($this->getDataFolder() . DIRECTORY_SEPARATOR . self::SETTINGS_CONFIG);
+		if(!is_array($settings)) {
+			throw new PluginException("Could not parse " . self::SETTINGS_CONFIG . ", please check the syntax and restart the server.");
+		}
+		$this->loadConfiguration($settings);
 	}
 
+	/**
+	 * @param array<string, array<string, bool|int|string|array<string, bool|int|string|array<string, bool|int|string|array<string, string>>>>> $settings
+	 */
 	private function loadConfiguration(array $settings) : void {
 		$eventConfig = $settings["general"]["events"];
 
-		if(self::getConfigBool($eventConfig["block-break"] ?? false)) {
-			$this->registerListener(new BlockBreakAutoPickup);
-		}
-
-		if(self::getConfigBool($eventConfig["entity-death"] ?? false)) {
-			$this->registerListener(new EntityDeathAutoPickup);
-		}
-
-		if(self::getConfigBool($eventConfig["player-death"] ?? false)) {
-			$this->registerListener(new PlayerDeathPickup);
-		}
-
-		if(self::getConfigBool($eventConfig["entity-explosion"] ?? false)) {
-			$this->registerListener(new EntityExplosionPickup);
-		}
-
-		$inventoryConfig = $eventConfig["inventory-full"] ?? null;
-		if($inventoryConfig !== null && self::getConfigBool($inventoryConfig["active"])) {
-			$this->registerListener(new InventoryFullAlert(
-				$inventoryConfig["interval"],
-				TextFormat::colorize($inventoryConfig["message"]["text"] ?? ''),
-				TextFormat::colorize($inventoryConfig["message"]["secondary-text"] ?? ''),
-				strtolower($inventoryConfig["message"]["type"] ?? "message"),
-				$inventoryConfig["sound"] ?? false)
-			);
+		foreach(self::OPTIONAL_LISTENERS as $configOption => $listenerClass) {
+			$this->registerOptionalListener($configOption, $eventConfig, $listenerClass);
 		}
 
 		$remainingItemsConfig = $eventConfig["remaining-items"] ?? "drop";
 		if($remainingItemsConfig === "drop") {
 			$this->registerListener(new DropItemsAfterPickup);
+		}
+
+		$inventoryConfig = $eventConfig["inventory-full"] ?? null;
+		if(!is_array($inventoryConfig)) {
+			throw new PluginException("Invalid config settings for general.events.inventory-full");
+
+		}
+		$inventoryConfigActive = $inventoryConfig["active"] ?? false;
+		$inventoryConfigSound = $inventoryConfig["sound"] ?? false;
+		if(is_array($inventoryConfigActive) || is_array($inventoryConfigSound)) {
+			throw new PluginException("Invalid config settings for general.events.inventory-full");
+		}
+		if(self::getConfigBool($inventoryConfigActive)) {
+			$this->registerListener(new InventoryFullAlert(
+					(int)($inventoryConfig["interval"] ?? 5),
+					TextFormat::colorize($inventoryConfig["message"]["text"] ?? ''),
+					TextFormat::colorize($inventoryConfig["message"]["secondary-text"] ?? ''),
+					strtolower($inventoryConfig["message"]["type"] ?? "message"),
+					self::getConfigBool($inventoryConfigSound))
+			);
+		}
+	}
+
+	/**
+	 * @param string                                                                                              $configOption
+	 * @param array<string, array<string, array<string, string>|bool|int|string>|bool|int|string>|bool|int|string $configSettings
+	 * @param class-string<\pocketmine\event\Listener>                                                            $listenerClass
+	 */
+	private function registerOptionalListener(string $configOption, mixed $configSettings, string $listenerClass) : void {
+		$opt = $configSettings[$configOption] ?? false;
+		if(is_array($opt)) {
+			throw new PluginException("Invalid config settings for general.events." . $configOption);
+		}
+		if(self::getConfigBool($opt)) {
+			$this->registerListener(new $listenerClass);
 		}
 	}
 
